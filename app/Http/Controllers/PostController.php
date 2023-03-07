4ea42008c511e0fc65742c2da772492c096d1e14
anujababy1 <email@example.com>
2023-03-07 15:44:37 -0500

    Polymorphic Relationships : One To One

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function create(){
+
+        // $post = Post::create([
+        //     'title'=>'jjjjj'
+        // ]);
+        // $post->image()->create([
+        //     'url'=>'kkkk'
+        // ]);
+
+        // dd(Post::first()->image->url);
+
+
+        $user = User::first();
+        $user->image()->create([
+            'url'=>'ss'
+        ]);
+
+        dd($user->image->url);
         
         return view('posts.create');
     }
+    
 }
